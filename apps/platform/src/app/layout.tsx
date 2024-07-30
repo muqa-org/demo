02ab4d6024e0a9bf119a6192755a730646529dc8
kkatusic <kkatusic@gmail.com>
2024-07-30 10:38:37 +0200

    feature: added dmSans font and .prettierrc config file

--- apps/platform/src/app/layout.tsx
+++ apps/platform/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { DM_Sans } from 'next/font/google';
 import '@allo/kit/styles.css';
 import './globals.css';
 
@@ -7,27 +7,29 @@ import { WalletStatus } from './WalletStatus';
 import { Header } from './header';
 import { AlloKitProviders } from './providers';
 
-const inter = Inter({ subsets: ['latin'] });
+const dmSans = DM_Sans({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 export const metadata: Metadata = {
-  title: 'MUQA initiative Demo App test1',
-  description: '',
+	title: 'MUQA initiative Demo App test1',
+	description: '',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AlloKitProviders>
-          <Header />
-          <WalletStatus/>
-          <main className="max-w-screen-lg mx-auto py-16">{children}</main>
-        </AlloKitProviders>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='hr'>
+			<body className={dmSans.className}>
+				<AlloKitProviders>
+					<Header />
+					<WalletStatus />
+					<main className='max-w-screen-lg mx-auto py-16'>
+						{children}
+					</main>
+				</AlloKitProviders>
+			</body>
+		</html>
+	);
 }
