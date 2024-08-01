333989daf111c30408573af69fbad2fd41be8fb3
kkatusic <kkatusic@gmail.com>
2024-08-01 10:13:56 +0200

    fix: box shadow on round status

--- apps/platform/src/app/components/homepage/HomepageRoundBoxes.tsx
+++ apps/platform/src/app/components/homepage/HomepageRoundBoxes.tsx
@@ -2,8 +2,8 @@ import { RoundStatusBox } from '../RoundStatusBox';
 
 export function HomepageRoundBoxes() {
 	return (
-		<div className='mx-5 flex flex-col items-center justify-center xl:mx-48 xl:max-w-7xl 2xl:max-w-7xl'>
-			<div className='mt-24 flex w-full items-center justify-center gap-2 rounded-2xl bg-white p-5'>
+		<div className='mx-auto flex w-full flex-col items-center justify-center xl:mx-48 xl:max-w-7xl 2xl:max-w-7xl'>
+			<div className='mt-24 flex w-full items-center justify-center gap-2 rounded-2xl bg-white p-5 shadow-[0_8px_18px_rgba(0,0,0,0.08),0_0px_1px_rgba(0,0,0,0.05)]'>
 				<RoundStatusBox
 					title='PRIJAVE'
 					dateTime='25.7. - 6.8.2024. | 12:00 h'
