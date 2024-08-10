323c4fface9241125a45ea4d2c939cad6238b803
kkatusic <kkatusic@gmail.com>
2024-08-10 17:03:53 +0200

    feat: homepage stats line

---
+++ apps/platform/src/app/components/homepage/HomepageStats.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Image from 'next/image';
+import { useTranslations } from 'next-intl';
+
+import images from '@/app/components/common/Images';
+import Link from 'next/link';
+
+export function HomepageStats() {
+	const t = useTranslations('home');
+
+	return (
+		<div className='mt-14 bg-cover bg-center bg-no-repeat pb-16 leading-normal'>
+			<div className='mx-auto flex flex-col xl:max-w-7xl 2xl:max-w-[1440px]'>
+				<div className='mb-20 h-[1px] border-2 border-dashed border-borderGray'></div>
+				<h2 className='mb-5 text-center text-4xl font-normal text-primaryBlack'>
+					{t('stats')}
+				</h2>
+				<div className='text-grayDark mx-10 mt-10 grid grid-cols-1 gap-[17em] py-6 text-base font-normal md:grid-cols-3'>
+					<div className='col-span-1 text-center'>
+						<h3 className='mb-6 text-[45px] font-bold text-[#39A56A]'>
+							50.000€
+						</h3>
+						<div>{t('statsBudget')}</div>
+					</div>
+
+					<div className='col-span-1 text-center'>
+						<h3 className='mb-6 text-[45px] font-bold text-[#39A56A]'>
+							25 projects
+						</h3>
+						<div>{t('statsAccepted')}</div>
+						<Link href='/' className='underline hover:text-blue'>
+							{t('statsLearn')}
+						</Link>
+					</div>
+
+					<div className='col-span-1 text-center'>
+						<h3 className='mb-6 text-[45px] font-bold text-[#39A56A]'>
+							20 days
+						</h3>
+						<div>{t('statsPeriod')}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
--- apps/platform/src/app/green/page.tsx
+++ apps/platform/src/app/green/page.tsx
@@ -2,12 +2,14 @@ import { HomepageExampleProject } from '@/app/components/homepage/HomepageExampl
 import { HomepageHero } from '@/app/components/homepage/HomepageHero';
 import { HomepageIntro } from '@/app/components/homepage/HomepageIntro';
 import { HomepageRoundBoxes } from '@/app/components/homepage/HomepageRoundBoxes';
+import { HomepageStats } from '@/app/components/homepage/HomepageStats';
 
 export default function HomeGreen() {
 	return (
 		<>
 			<HomepageHero />
 			<HomepageExampleProject />
+			<HomepageStats />
 			<HomepageIntro />
 			<HomepageRoundBoxes />
 		</>
