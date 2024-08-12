cf2a2a5b9a9296c8ef62319ce8b26c4892f85547
kkatusic <kkatusic@gmail.com>
2024-08-13 00:07:51 +0200

    fix: removed obvious comment

--- apps/platform/src/i18n.ts
+++ apps/platform/src/i18n.ts
@@ -2,7 +2,6 @@ import { cookies } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async () => {
-	// Access the `lang` cookie
 	const cookieStore = cookies();
 	const locale = cookieStore.get('lang')?.value || 'en';
 
