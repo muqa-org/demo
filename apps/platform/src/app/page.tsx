a007054bca2b3c068f1e0ea0bd1edf7cf77dbf00
kkatusic <kkatusic@gmail.com>
2024-08-13 00:06:50 +0200

    fix: removed unused import and function

--- apps/platform/src/app/page.tsx
+++ apps/platform/src/app/page.tsx
@@ -1,10 +1,7 @@
-import { useTranslations } from 'next-intl';
-
 import HomepageIntro from '@/app/components/homepage/HomepageIntro';
 import HomepageRoundBoxes from '@/app/components/homepage/HomepageRoundBoxes';
 
 export default function Home() {
-	const t = useTranslations();
 
 	return (
 		<div className='bg-[#F0F7FE] pb-36 pt-20'>
@@ -12,8 +9,4 @@ export default function Home() {
 			<HomepageRoundBoxes />
 		</div>
 	);
-}
-
-function fixSvg(svg: string) {
-	return svg.replace(/(width|height)="[^"]*"/g, '$1="100%"');
 }
