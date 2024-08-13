8b5cd6e96ad42a86fd97b42d60efc4ad5464583a
kkatusic <kkatusic@gmail.com>
2024-08-13 19:26:11 +0200

    fix: sidebar round phase translate (#39)

--- apps/platform/src/app/components/projects/ProjectsSidebarPhases.tsx
+++ apps/platform/src/app/components/projects/ProjectsSidebarPhases.tsx
@@ -1,26 +1,30 @@
+import { useTranslations } from 'next-intl';
+
 import RoundPhase from '@/app/components/RoundPhase';
 import { RoundData } from '@/app/types/round';
 
 export default function ProjectsSidebarPhases({ round }: { round: RoundData }) {
+	const t = useTranslations('round');
+
 	return (
 		<div className='mt-6'>
 			<RoundPhase
-				title='PRIJAVE'
+				title={t('roundApplication')}
 				startDate={new Date('2024-07-25T12:00:00')}
 				endDate={new Date('2024-08-06T12:00:00')}
 			/>
 			<RoundPhase
-				title='DONIRANJE'
+				title={t('roundDonation')}
 				startDate={new Date('2024-08-07T12:00:00')}
 				endDate={new Date('2024-09-07T12:00:00')}
 			/>
 			<RoundPhase
-				title='ZBRAJANJE GLASOVA'
+				title={t('roundVotes')}
 				startDate={new Date('2024-09-08T12:00:00')}
 				endDate={new Date('2024-09-13T12:00:00')}
 			/>
 			<RoundPhase
-				title='REZULTATI'
+				title={t('roundResults')}
 				startDate={new Date('2024-09-15T12:00:00')}
 				endDate={new Date('2024-09-15T12:00:00')}
 			/>
