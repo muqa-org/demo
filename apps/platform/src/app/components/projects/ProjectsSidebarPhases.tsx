4e09f562851f7f1c24945f8da36d66f151fee1e3
kkatusic <kkatusic@gmail.com>
2024-08-15 15:29:02 +0200

    Fix: round phase names and dates (#46)

--- apps/platform/src/app/components/projects/ProjectsSidebarPhases.tsx
+++ apps/platform/src/app/components/projects/ProjectsSidebarPhases.tsx
@@ -10,23 +10,23 @@ export default function ProjectsSidebarPhases({ round }: { round: RoundData }) {
 		<div className='mt-6'>
 			<RoundPhase
 				title={t('roundApplication')}
-				startDate={new Date('2024-07-25T12:00:00')}
-				endDate={new Date('2024-08-06T12:00:00')}
+				startDate={new Date('2024-08-19T12:00:00')}
+				endDate={new Date('2024-09-02T12:00:00')}
 			/>
 			<RoundPhase
 				title={t('roundDonation')}
-				startDate={new Date('2024-08-07T12:00:00')}
-				endDate={new Date('2024-09-07T12:00:00')}
+				startDate={new Date('2024-09-16T12:00:00')}
+				endDate={new Date('2024-09-30T12:00:00')}
 			/>
 			<RoundPhase
 				title={t('roundVotes')}
-				startDate={new Date('2024-09-08T12:00:00')}
-				endDate={new Date('2024-09-13T12:00:00')}
+				startDate={new Date('2024-10-01T12:00:00')}
+				endDate={new Date('2024-10-10T12:00:00')}
 			/>
 			<RoundPhase
 				title={t('roundResults')}
-				startDate={new Date('2024-09-15T12:00:00')}
-				endDate={new Date('2024-09-15T12:00:00')}
+				startDate={new Date('2024-10-15T12:00:00')}
+				endDate={new Date('2024-30-11T12:00:00')}
 			/>
 		</div>
 	);
