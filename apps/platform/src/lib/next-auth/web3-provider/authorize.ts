5ace664353155d4992ac8c3d84d6a4521c250a21
Ante Borzić <aborzic@extensionengine.com>
2024-08-26 19:27:48 +0200

    chore: remove logs

--- apps/platform/src/lib/next-auth/web3-provider/authorize.ts
+++ apps/platform/src/lib/next-auth/web3-provider/authorize.ts
@@ -6,14 +6,12 @@ export default async function authorize(
   credentials: Record<'address' | 'signedNonce', string> | undefined,
   req: Pick<RequestInternal, 'body' | 'headers' | 'method' | 'query'>
 ) {
-  console.log('authorizing crypto', credentials);
   if (!credentials) return null;
 
   const { address, signedNonce } = credentials;
 
   // Get user from database with their generated nonce
   const user = await getUserWithNonce(address);
-  console.log('user', user);
 
   if (!user?.authNonce) return null;
 
