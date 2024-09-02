968443f77432e584798400489432d93d4ec07191
kkatusic <kkatusic@gmail.com>
2024-09-02 13:57:02 +0200

    added test script for creating user and topic

--- apps/platform/src/app/[chainId]/projects/create/actions.ts
+++ apps/platform/src/app/[chainId]/projects/create/actions.ts
@@ -1,12 +1,85 @@
 'use server';
 
-export async function createUserAction(formData) {
-  // Process formData here (e.g., save to a database)
-  console.log('Received form data:', formData);
+import { generatePassword } from '@/app/helpers/appHelpers';
 
-  // Simulate a delay for the example
-  await new Promise((resolve) => setTimeout(resolve, 500));
+export async function createProjectAction(prevState: any, formData: FormData) {
+	const apiUrl = process.env.NEXT_DISCOURSE_FORUM_URL || '';
+	const apiKey = process.env.NEXT_DISCOURSE_API_KEY || '';
+	const apiUsername = process.env.NEXT_DISCOURSE_USERNAME || '';
 
-  // Optionally, return some response
-  return { success: true, message: 'User created successfully!' };
+	const title = formData.get('title')?.toString().trim();
+	const description = formData.get('description')?.toString().trim();
+	const email = formData.get('email')?.toString().trim();
+
+  const newUserUsername = email?.split('@')[0] || '';
+
+	const userData = {
+		name: newUserUsername,
+		username: newUserUsername,
+		email: email || '',
+		password: generatePassword(),
+		active: true,
+		approved: true,
+	};
+
+	// First create user on forum
+	const responseUser = await fetch(`${apiUrl}/users.json`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Api-Key': apiKey!,
+			'Api-Username': apiUsername,
+		},
+		body: JSON.stringify(userData),
+	});
+
+	if (!responseUser.ok) {
+		const errorText = await responseUser.text();
+		console.error('Error creating user:', errorText);
+		return {
+			status: false,
+			message: `Failed to create user: ${responseUser.statusText}`,
+		};
+	} else {
+		// User created successfully, now create topic
+		const dataUser = await responseUser.json();
+
+		console.log('User created successfully:', dataUser);
+
+		const responseTopic = await fetch(`${apiUrl}/posts.json`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Api-Key': apiKey,
+				'Api-Username': newUserUsername,
+			},
+			body: JSON.stringify({
+				title: 'Prijedlog:' + title,
+				raw: description,
+				category: 8,
+			}),
+		});
+
+		if (!responseTopic.ok) {
+			const errorText = await responseTopic.text();
+			console.error('Error creating topic:', errorText);
+			return {
+				status: false,
+				message: `Failed to create topic: ${responseTopic.statusText}`,
+			};
+		}
+
+		const data = await responseTopic.json();
+		console.log('Topic created successfully:', data);
+
+    return {
+      status: true,
+      message: 'Topic created successfully!',
+    };
+	}
+
+	return {
+		status: true,
+		message: 'Topic created successfully!',
+	};
 }
--- apps/platform/src/app/[chainId]/projects/create/page.tsx
+++ apps/platform/src/app/[chainId]/projects/create/page.tsx
@@ -1,19 +1,79 @@
 'use client';
 
 import Container from '@/app/components/Container';
+import { useFormState, useFormStatus } from 'react-dom';
+
+import { createProjectAction } from './actions';
 
 export default function CreateProjectPage() {
+	const [state, formAction] = useFormState(createProjectAction, {
+		message: '',
+		status: '',
+	});
+	const { pending } = useFormStatus();
+
 	return (
 		<section className='py-4'>
-			<Container className='mx-auto mb-6 flex justify-between gap-10 px-5 py-5'>
+			<Container className='mx-auto mb-6 flex flex-wrap justify-between gap-10 px-5 py-5'>
 				<div className='flex h-full w-full flex-col flex-wrap justify-between'>
 					<h1 className='w-full border-b border-borderGrayLight pb-10 pt-10 text-[28px] font-normal leading-normal text-primaryBlack md:text-4xl'>
 						Predložite projekt
 					</h1>
 				</div>
-        <div>
-          
-        </div>
+				<div className='w-full'>
+					<form action={formAction} className='mx-auto max-w-lg space-y-6 p-4'>
+						<div>
+							<label
+								htmlFor='email'
+								className='block text-sm font-medium text-gray-700'
+							>
+								Email
+							</label>
+							<input
+								type='email'
+								id='email'
+								name='email'
+								className='border-gray-300 focus:border-indigo-500 mt-1 block w-full rounded-md border p-2 shadow-sm focus:ring-indigo-500 sm:text-sm'
+								required
+							/>
+						</div>
+						<div>
+							<label
+								htmlFor='title'
+								className='block text-sm font-medium text-gray-700'
+							>
+								Title
+							</label>
+							<input
+								type='text'
+								id='title'
+								name='title'
+								className='border-gray-300 focus:border-indigo-500 mt-1 block w-full rounded-md border p-2 shadow-sm focus:ring-indigo-500 sm:text-sm'
+								required
+							/>
+						</div>
+						<div>
+							<label
+								htmlFor='description'
+								className='block text-sm font-medium text-gray-700'
+							>
+								Description
+							</label>
+							<textarea
+								id='description'
+								name='description'
+								className='border-gray-300 focus:border-indigo-500 mt-1 block w-full rounded-md border p-2 shadow-sm focus:ring-indigo-500 sm:text-sm'
+								required
+							/>
+						</div>
+						<button
+							type='submit'
+							className='border-transparent inline-flex justify-center rounded-md border bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+						>
+							{pending ? 'Submitting...' : 'Submit'}
+						</button>
+					</form>
+				</div>
 			</Container>
 		</section>
 	);
