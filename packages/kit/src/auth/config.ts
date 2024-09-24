fe6b6c0d1771e5ae4dd061aa0f56cbcd5b1a0021
Ante Borzić <aborzic@extensionengine.com>
2024-09-24 16:19:04 +0200

    feat: remove unused config

--- packages/kit/src/auth/config.ts
+++
@@ -1,5 +0,0 @@
-import { SupportedNetworks } from '@cometh/connect-sdk';
-
-export const COMETH_API_KEY = process.env.NEXT_PUBLIC_COMETH_API_KEY!
-
-export const DEFAULT_COMETH_NETWORK = SupportedNetworks.ARBITRUM_SEPOLIA;
