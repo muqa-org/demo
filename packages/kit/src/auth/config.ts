3efabdda97a4c868ddf57a119e4ba5180b39484a
Ante Borzić <aborzic@extensionengine.com>
2024-09-24 10:27:15 +0200

    feat: swtich deafult chain to arbitrum sepolia

--- packages/kit/src/auth/config.ts
+++ packages/kit/src/auth/config.ts
@@ -2,4 +2,4 @@ import { SupportedNetworks } from '@cometh/connect-sdk';
 
 export const COMETH_API_KEY = process.env.NEXT_PUBLIC_COMETH_API_KEY!
 
-export const DEFAULT_COMETH_NETWORK = SupportedNetworks.FUJI;
+export const DEFAULT_COMETH_NETWORK = SupportedNetworks.ARBITRUM_SEPOLIA;
--- packages/kit/src/wagmi/providers/cometh.provider.tsx
+++ packages/kit/src/wagmi/providers/cometh.provider.tsx
@@ -3,17 +3,21 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
-import { avalancheFuji } from 'viem/chains';
+import { arbitrumSepolia } from 'viem/chains';
 import { Config, createConfig, http, WagmiProvider } from 'wagmi';
+
+import { comethConnector } from '..';
 
 const wagmiConfig = createConfig({
   chains: [
-    avalancheFuji
+    arbitrumSepolia
   ],
-  ssr: true,
+
   transports: {
-    [avalancheFuji.id]: http()
-  }
+    // [arbitrumSepolia.id]: http()
+    [arbitrumSepolia.id]: http('https://virtual.arbitrum-sepolia.rpc.tenderly.co/bd5d0ce2-fccd-45aa-b5a1-41ee94d12875')
+  },
+  connectors: [comethConnector],
 })
 
 const queryClient = new QueryClient();
